81d431f027d199340b479c54346832c46361501f
wbwu2021-oss <wbwu2021@gmail.com>
2026-04-04 22:56:45 -0700

    Update game.py

--- save-the-watermelon/src/game.py
+++ save-the-watermelon/src/game.py
@@ -2,4 +2,35 @@
 """
 import the themes from src.words and import the hiding, checking coding from src.logic
 """
+from src.words import Themes, get_word_from_theme
+from src.logic import mask_random_letters, check_guess
+def play_game():
+  print("select a theme:")
+  for key in Themes.items():
+    print(f"{key}.{value[0]}")
+choice=input("enter number(1,2,3):")
+secret_word=get_word_from_theme(choice)
+
+"""
+default setting
+"""
+display_list, hidden_indices=mask_random_letters(secret_word)
+slices=6
+
+"""
+guess the letter
+"""
+while slice>0
+ print(f"\nword:{''.join(display_list)}")
+
+ if check_guess(guess, secret_word, display_list, hidden_indices):
+   print("good job")
+ else:
+   slices=slices-1
+   print("please try again")
+
+
+
+
+
 
