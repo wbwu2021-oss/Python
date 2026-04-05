2895bd6f1c94dd39dbc029b51921fa91f646d25c
wbwu2021-oss <wbwu2021@gmail.com>
2026-04-04 21:57:03 -0700

    Implement random word selection from themes
    Added a function to select a random word from a theme.

--- save-the-watermelon/src/words.py
+++ save-the-watermelon/src/words.py
@@ -1,3 +1,5 @@
+```python
+
 import random
 """
 Word list with different themes
@@ -5,4 +7,14 @@ Word list with different themes
 
 # define the word list with different themes
 Themes={"1":("animals", ["tiger", "zebra", "rabbit", "wolf","panda"]),
-        "2"
+        "2":("food", ["bread", "pizza", "burger", "noodle"]),
+        "3":("sport", ["soccer", "tennis", "bedminton", "surfing"，“skate"]}
+
+def get_world_from_theme(choice):
+"""
+select a random word with a theme
+"""
+if choice in Themes:
+    return random.choice(Themes[choice][1])
+return None
+```
