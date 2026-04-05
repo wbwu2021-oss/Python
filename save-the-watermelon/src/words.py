ffaf13a8f64d66cf80840a32fc2ca7acd7d1723f
wbwu2021-oss <wbwu2021@gmail.com>
2026-04-04 21:49:21 -0700

    Update words.py

--- save-the-watermelon/src/words.py
+++ save-the-watermelon/src/words.py
@@ -1,1 +1,8 @@
+import random
+"""
+Word list with different themes
+"""
 
+# define the word list with different themes
+Themes={"1":("animals", ["tiger", "zebra", "rabbit", "wolf","panda"]),
+        "2"
