7c3a226e5cd9c62ab9c9df193000aa1eebdecfdc
wbwu2021-oss <wbwu2021@gmail.com>
2026-04-05 21:27:39 -0700

    Update test_logic.py

--- save-the-watermelon/tests/test_logic.py
+++ save-the-watermelon/tests/test_logic.py
@@ -1,1 +1,10 @@
 
+from src.logic import mask_random_letters
+def test_masking_length():
+  word="panda"
+  display,hidden=mask_random_letters(word)
+  assert len(display)==len(word)
+  assert len(hidden)in [2,3]
+  print("passed")
+if __name__=="__main__"
+test_masking_length()
