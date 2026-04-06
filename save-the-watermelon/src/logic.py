e0c32c28ab6739974d0cd25d5d35a57fd66f99d3
wbwu2021-oss <wbwu2021@gmail.com>
2026-04-05 23:05:32 -0700

    Fix syntax errors and improve masking logic

--- save-the-watermelon/src/logic.py
+++ save-the-watermelon/src/logic.py
@@ -2,24 +2,28 @@
 
 ```python
 import random
+
 def mask_random_letters(word):
-  word_list=list(word)
-  num_to_hide=random.randint(2,3)
-  hidden_indices=random.sample(range(len(word))), num_to_hide)
+    word_list=list(word)
+    num_to_hide=random.randint(2,3)
+  
+    hidden_indices=random.sample(range(len(word)), num_to_hide)
   
   display_list=[]
   for i in range(len(word)):
-    if i in hidden_indices:
-      display_list.append("_")
-    else:
-      display_list.append(word[i])
+      if i in hidden_indices:
+          display_list.append("_")
+      else:
+          display_list.append(word[i])
 
   return display_list, hidden_indices
 
 def check-guess(guess, secret_word, displau_list, hidden_indices):
+     found=False
+
     for i in hidden_indices:
-      if secret_word[i]==guess;
-         display-list[i]=guess
+      if secret_word[i]==guess:
+         display_list[i]=guess
          found=True
     return found
 ```
