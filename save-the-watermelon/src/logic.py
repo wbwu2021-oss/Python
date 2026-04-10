65cb2da757c0288d8754d36e313229c9e8571f09
wbwu2021-oss <wbwu2021@gmail.com>
2026-04-09 21:05:04 -0700

    Refactor masking and guessing logic in logic.py
    Refactor mask_random_letters and check_guess functions to improve readability and ensure correct logic for hiding letters and checking guesses.

--- save-the-watermelon/src/logic.py
+++ save-the-watermelon/src/logic.py
@@ -1,30 +1,42 @@
-# hiding 2 or 3 letters and check the answer
 
-```python
 import random
 
 def mask_random_letters(word):
+    """
+    hide 2 or 3 letters
+    """
+    word = word.lower()
     word_list=list(word)
-    num_to_hide=random.randint(2,3)
+# avoid the length of the secret word beyond the word
+
+    num_to_hide=min(random.randint(2,3), len(word))
   
     hidden_indices=random.sample(range(len(word)), num_to_hide)
   
-  display_list=[]
-  for i in range(len(word)):
-      if i in hidden_indices:
-          display_list.append("_")
-      else:
-          display_list.append(word[i])
+    display_list=[]
+    for i in range(len(word)):
+        if i in hidden_indices:
+            display_list.append("_")
+        else:
+            display_list.append(word_list[i])
 
-  return display_list, hidden_indices
-
+    return display_list, hidden_indices
+"""
+check whether guessd letter is right or wrong.
+"""
 def check_guess(guess, secret_word, display_list, hidden_indices):
-     found=False
+    guess = guess.lower()
+    secret_word = secret_word.lower()
+
+    found=False
 
     for i in hidden_indices:
-      if secret_word[i]==guess:
-         display_list[i]=guess
-         found=True
+        if secret_word[i]==guess and display_list[i]=="_":
+            display_list[i]=guess
+            found=True
     return found
-```
+
+def is_word_complete(display_list):
+    return"_" not in display_list
+
 
